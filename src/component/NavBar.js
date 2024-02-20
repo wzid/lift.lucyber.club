bfd2e8633dd6fe29433c31a0af8e2a72503fe4cf
simplisambo <sbodine@liberty.edu>
2024-02-19 20:22:34 -0500

    Change navbar heading

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -6,6 +6,7 @@ import { AuthContext } from "./AuthProvider";
 import { usePathname } from "next/navigation";
 import { Icon } from "@iconify/react";
 import clsx from 'clsx';
+import luCyberLogo from "../../public/images/lu_cyber_logo.svg";
 
 export default function NavBar() {
   const { currentUser } = useContext(AuthContext);
@@ -33,7 +34,10 @@ export default function NavBar() {
   return (
     <div className="w-full flex justify-between items-center pb-12 md:pb-12 sm:text-base md:text-lg mt-4">
       <Link href="/" className="hover:opacity-70 transition-opacity">
-        <h1 className="text-3xl">🏋️‍♂️ <span className="hidden sm:inline">cyber lifts</span></h1>
+        <h1 className="text-3xl flex items-center">
+          <img src="/images/lu_cyber_logo.svg" alt="cyber lifts logo" className="size-10 mt-2" />
+          <span className="hidden sm:inline ml-2">Cyber <span className="font-light">Lifts</span></span>
+        </h1>
       </Link>
       <div className="flex gap-3 sm:gap-6 text-lg">
         {navLinks.map(({ name, path }) => {
